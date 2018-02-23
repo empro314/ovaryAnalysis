a6f52416d9b2039fca90c08ffe9f8454b5432bd7
Marcin Kaszkowiak <mgkaszkowiak@gmail.com>
2018-02-23 11:22:51 +0100

    final

--- rAnalysis/dataManip.R
+++ rAnalysis/dataManip.R
@@ -1,1 +1,12 @@
-corr
+library(haven)
+Data <- read_sav("/media/empro/5C02C2EB02C2C8EA/Ubuntu/Fendler/ovaryAnalysis/spAn/data.sav")
+
+
+Labels <- Data[c(1)]
+Data <- Data[-c(1)]
+
+corMatrix <- cor(Data, method = 'spearman')
+
+#heatm <- heatmap(corMatrix)
+heatm <- heatmap(corMatrix, Rowv = NA, Colv = NA)
+
